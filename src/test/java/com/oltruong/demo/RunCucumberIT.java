17642b53490e253feba5df574eccf6ad6d513016
Olivier Truong <olivier@nadeva.fr>
2022-11-04 23:38:19 +0100

    Fix cucumber option

--- src/test/java/com/oltruong/demo/RunCucumberIT.java
+++ src/test/java/com/oltruong/demo/RunCucumberIT.java
@@ -11,9 +11,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-//@CucumberOptions(features = "classpath:features", format = {"html:target/cucumber-html-report","json:target/cucumber.json"})
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,json:target/cucumber.json,html:target/cucumber-html-report.html")
 public class RunCucumberIT {
-
-
 }
